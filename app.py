c9bfb685bc384685ed5cb6fba23dd291c4cb2658
sunnyCodeENGINEER <116773072+sunnyCodeENGINEER@users.noreply.github.com>
2024-08-24 10:11:34 +0000

    update to app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 import os
+from urllib.parse import urlparse
 
 from flask import Flask, request, jsonify
 from extract_text import extract_text_from_pdf, extract_text_from_docx, extract_text_from_pptx
+
+import requests
 import subprocess
 
 app = Flask(__name__)
+
+
+def download_file(url):
+    local_filename = url.split('/')[-1]
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return local_filename
 
 
 @app.route('/run_code', methods=['POST'])
@@ -12,8 +25,22 @@ def run_code():
     data = request.get_json()
     file_path = data.get('file_path')
 
-    if not file_path or not os.path.exists(file_path):
+    # if not file_path or not os.path.exists(file_path):
+    #     return jsonify({'response': 'File not found'}), 400
+
+    if not file_path:
         return jsonify({'response': 'File not found'}), 400
+
+    # Check if the file path is a URL
+    parsed_url = urlparse(file_path)
+    if parsed_url.scheme in ['http', 'https']:
+        try:
+            file_path = download_file(file_path)
+        except Exception as e:
+            return jsonify({'response': f'Error downloading file: {str(e)}'}), 400
+    else:
+        if not os.path.exists(file_path):
+            return jsonify({'response': 'File not found'}), 400
 
     _, file_extension = os.path.splitext(file_path)
 
